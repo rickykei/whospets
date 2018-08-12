45fe93b9812b8b0372d1c52d6b6dcf0cbd337465
Joanne Wong <joanne0802@gmail.com>
2018-08-13 00:14:19 +0800

    signup
    signup

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -11,6 +11,11 @@ import { FacebookLoginService } from '../facebook-login/facebook-login.service';
 import { GoogleLoginService } from '../google-login/google-login.service';
 import { TwitterLoginService } from '../twitter-login/twitter-login.service';
 
+
+import {Http} from '@angular/http';
+import 'rxjs/add/operator/map';
+import { LoginPage } from '../login/login';
+
 @Component({
   selector: 'signup-page',
   templateUrl: 'signup.html'
@@ -19,7 +24,11 @@ export class SignupPage {
   signup: FormGroup;
   main_page: { component: any };
   loading: any;
+  success: string;
   
+
+  fb_email : string;
+  fb_uid: string;
   username: AbstractControl;
 
   constructor(
@@ -28,6 +37,7 @@ export class SignupPage {
     public facebookLoginService: FacebookLoginService,
     public googleLoginService: GoogleLoginService,
     public twitterLoginService: TwitterLoginService,
+    private http: Http,
     public loadingCtrl: LoadingController
   ) {
     this.main_page = { component: TabsNavigationPage };
@@ -35,15 +45,39 @@ export class SignupPage {
     this.signup = new FormGroup({
       username : new FormControl(''),
       email: new FormControl('', Validators.required),
-      password: new FormControl('test', Validators.required),
-      confirm_password: new FormControl('test', Validators.required)
+      password: new FormControl('', Validators.required),
+      confirm_password: new FormControl('', Validators.required)
     });
 
     this.username =  this.signup.controls['username'];
   }
 
   doSignup(){
-    this.nav.setRoot(this.main_page.component);
+    let data = this.signup.value;
+ 
+    console.log('-------------------doSignup');
+    //http://api.whospets.com/api/users/signup.php?logintype=fb&username=rickykei@yahoo.com.hk&password=1234
+     var url = 'http://api.whospets.com/api/users/signup.php?logintype=normal&username=' + data.email + '&password='+data.password ;
+     console.log(url);
+
+     this.http.get(url).map(res => res.json()).subscribe(data2 => {
+ 
+        console.log(data2.success);
+    
+          this.success = data2.success;
+          console.log(this.success);
+    
+          if(this.success=='true')
+          {
+            console.log('inside true');
+            this.nav.setRoot(LoginPage);    
+          }
+          else
+          {
+            console.log('inside false'); 
+          }
+        });
+    this.nav.setRoot(LoginPage);
   }
 
   doFacebookSignup() {
@@ -53,67 +87,91 @@ export class SignupPage {
     let env = this;
 
     this.facebookLoginService.getFacebookUser()
-    .then(function(data) {
+    .then(data => {
+    
+      console.log('-------------------doSignup');
+      //http://api.whospets.com/api/users/signup.php?logintype=fb&username=rickykei@yahoo.com.hk&password=1234
+       var url = 'http://api.whospets.com/api/users/signup.php?logintype=fb&username=' + data.email + '&uid='+data.userId ;
+       console.log(url);
+       this.http.get(url).map(res => res.json()).subscribe(data2 => {
+ 
+        console.log(data2.success);
+    
+          this.success = data2.success;
+          console.log(this.success);
+    
+          if(this.success=='true')
+          {
+            console.log('inside true');
+            this.nav.setRoot(LoginPage);    
+          }
+          else
+          {
+            console.log('inside false'); 
+          }
+        });
+        env.nav.setRoot(LoginPage);
        // user is previously logged with FB and we have his data we will let him access the app
-      env.nav.setRoot(env.main_page.component);
     }, function(error){
       //we don't have the user data so we will ask him to log in
       env.facebookLoginService.doFacebookLogin()
       .then(function(res){
         env.loading.dismiss();
-        env.nav.setRoot(env.main_page.component);
+       // env.nav.setRoot(env.main_page.component);
       }, function(err){
         console.log("Facebook Login error", err);
         env.loading.dismiss();
       });
+
+      
     });
   }
 
-  doTwitterSignup() {
-    this.loading = this.loadingCtrl.create();
-    // Here we will check if the user is already logged in
-    // because we don't want to ask users to log in each time they open the app
-    let env = this;
+  // doTwitterSignup() {
+  //   this.loading = this.loadingCtrl.create();
+  //   // Here we will check if the user is already logged in
+  //   // because we don't want to ask users to log in each time they open the app
+  //   let env = this;
 
-    this.twitterLoginService.getTwitterUser()
-    .then(function(data) {
-       // user is previously logged with FB and we have his data we will let him access the app
-      env.nav.setRoot(env.main_page.component);
-    }, function(error){
-      //we don't have the user data so we will ask him to log in
-      env.twitterLoginService.doTwitterLogin()
-      .then(function(res){
-        env.loading.dismiss();
-        env.nav.setRoot(env.main_page.component);
-      }, function(err){
-        console.log("Facebook Login error", err);
-        env.loading.dismiss();
-      });
-    });
-  }
+  //   this.twitterLoginService.getTwitterUser()
+  //   .then(function(data) {
+  //      // user is previously logged with FB and we have his data we will let him access the app
+  //     env.nav.setRoot(env.main_page.component);
+  //   }, function(error){
+  //     //we don't have the user data so we will ask him to log in
+  //     env.twitterLoginService.doTwitterLogin()
+  //     .then(function(res){
+  //       env.loading.dismiss();
+  //       env.nav.setRoot(env.main_page.component);
+  //     }, function(err){
+  //       console.log("Facebook Login error", err);
+  //       env.loading.dismiss();
+  //     });
+  //   });
+  // }
 
-  doGoogleSignup() {
-    this.loading = this.loadingCtrl.create();
+  // doGoogleSignup() {
+  //   this.loading = this.loadingCtrl.create();
 
-    // Here we will check if the user is already logged in because we don't want to ask users to log in each time they open the app
-    let env = this;
+  //   // Here we will check if the user is already logged in because we don't want to ask users to log in each time they open the app
+  //   let env = this;
 
-    this.googleLoginService.trySilentLogin()
-    .then(function(data) {
-       // user is previously logged with Google and we have his data we will let him access the app
-      env.nav.setRoot(env.main_page.component);
-    }, function(error){
-      //we don't have the user data so we will ask him to log in
-      env.googleLoginService.doGoogleLogin()
-      .then(function(res){
-        env.loading.dismiss();
-        env.nav.setRoot(env.main_page.component);
-      }, function(err){
-        console.log("Google Login error", err);
-        env.loading.dismiss();
-      });
-    });
-  }
+  //   this.googleLoginService.trySilentLogin()
+  //   .then(function(data) {
+  //      // user is previously logged with Google and we have his data we will let him access the app
+  //     env.nav.setRoot(env.main_page.component);
+  //   }, function(error){
+  //     //we don't have the user data so we will ask him to log in
+  //     env.googleLoginService.doGoogleLogin()
+  //     .then(function(res){
+  //       env.loading.dismiss();
+  //       env.nav.setRoot(env.main_page.component);
+  //     }, function(err){
+  //       console.log("Google Login error", err);
+  //       env.loading.dismiss();
+  //     });
+  //   });
+  // }
 
   showTermsModal() {
     let modal = this.modal.create(TermsOfServicePage);
