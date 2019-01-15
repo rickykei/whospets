ffc81d3020fd600e5259d79cb0a6ca028fc3ddbb
Joanne Wong <joanne0802@gmail.com>
2019-01-15 23:36:12 +0800

    Update src/pages/post-info/post-info.ts

--- src/pages/post-info/post-info.ts
+++ src/pages/post-info/post-info.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { PetDetailsModel } from '../profile/profile.model';
 import { SocialSharing } from '../../../node_modules/@ionic-native/social-sharing';
+import { PagesDisplayServiceProvider } from '../display/display.services';
+import { NativeStorage } from '@ionic-native/native-storage';
 
 /**
  * Generated class for the PostInfoPage page.
@@ -17,11 +19,13 @@ import { SocialSharing } from '../../../node_modules/@ionic-native/social-sharin
 export class PostInfoPage {
 
   post: PetDetailsModel = new PetDetailsModel();
-  uid:string;
+  user_id:string;
   
   constructor(public navCtrl: NavController,
      public navParams: NavParams,
-     public socialSharing: SocialSharing
+     public PagesDisplayServiceProvider: PagesDisplayServiceProvider,
+     public socialSharing: SocialSharing,
+     public nativeStorage: NativeStorage
     ) {
     this.post = navParams.get('post'); 
 
@@ -29,6 +33,12 @@ export class PostInfoPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PostInfoPage');
+
+    this.nativeStorage.getItem('profile_user_id')
+    .then(data => {
+        this.user_id = data.profile_user_id;
+         console.log(data.profile_user_id);
+      });
   }
 
   sharePost(post) {
@@ -41,5 +51,16 @@ export class PostInfoPage {
     .catch(() => {
        console.log('Error');
     });
+  }
+
+  likePost(post)
+  {
+    this.PagesDisplayServiceProvider.setlike(this.user_id, post.id, 'app_post')
+      .then(response => {
+        if(response.success==='true')
+        {
+          this.post.likecnt = this.post.likecnt+1;
+        }
+      });
   }
 }
