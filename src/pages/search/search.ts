1bb4d2ff13550e5538eff40272cc490176e2f0a6
Joanne Wong <joanne0802@gmail.com>
2019-03-24 00:42:26 +0800

    fix search commet,like,share & loading issue

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -3,6 +3,10 @@ import { NavController, NavParams, LoadingController } from 'ionic-angular';
 import { FeedService } from '../feed/feed.service';
 import { FeedPostModel } from '../feed/feed.model';
 import { PetinfoPage } from '../petinfo/petinfo';
+import { PagesDisplayServiceProvider } from '../display/display.services';
+import { SocialSharing } from '@ionic-native/social-sharing';
+import { NativeStorage } from '@ionic-native/native-storage';
+import { CommentpetPage } from '../commentpet/commentpet';
 
 /**
  * Generated class for the SearchPage page.
@@ -18,14 +22,31 @@ import { PetinfoPage } from '../petinfo/petinfo';
 export class SearchPage {
   details: Array<FeedPostModel> = new Array<FeedPostModel>() ;
   loading: any;
+  user_id: string;
+
+  likevalue : number;
+  dislikevalue : number;
+
   constructor(public navCtrl: NavController, 
     public loadingCtrl: LoadingController,
     public feedService: FeedService,
+    public PagesDisplayServiceProvider:PagesDisplayServiceProvider,
+    public socialSharing: SocialSharing,
+    public nativeStorage:NativeStorage,
     public navParams: NavParams) {
+
+      this.likevalue = 0;
+      this.dislikevalue = 0;
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SearchPage');
+
+    this.nativeStorage.getItem('profile_user_id')
+  .then(data => {
+      this.user_id = data.profile_user_id;
+    });
+
   }
 
   onSearch(event){
@@ -40,8 +61,16 @@ export class SearchPage {
     this.feedService
     .getSearchFeeds(url)
     .then(response => {
-      this.details = response.data.pets;
-      this.dismissLoading();
+      if(response.success === 'true')
+      {
+        this.details = response.data.pets;
+        this.loading.dismiss();
+      }
+      else
+      {
+        this.loading.dismiss();
+        alert("No related record, please try again.")
+      }
     });  
  }
 
@@ -57,7 +86,7 @@ export class SearchPage {
   }
 
   clearSearch(event){
-    console.info('onCancel: '+event.target.value);
+    console.info('clearSearch: '+event.target.value);
     this.refreshFeed();
   }
 
@@ -81,4 +110,50 @@ export class SearchPage {
   }, 2000);
   }
 
+  likePost(post)
+  {
+    if(post.ownlike==0)
+    {
+    this.PagesDisplayServiceProvider.setlike(this.user_id, post.product_id, 'shop_products')
+      .then(response => {
+        if(response.success==='true')
+        {
+          this.likevalue = post.likecnt;
+          this.likevalue ++;
+          post.likecnt = this.likevalue;
+          post.ownlike = 1;
+        }
+      });
+    }else{
+      this.PagesDisplayServiceProvider.setdislike(this.user_id, post.product_id, 'shop_products')
+      .then(response => {
+        if(response.success==='true')
+        {
+          this.dislikevalue = post.likecnt;
+          this.dislikevalue --;
+          post.likecnt = this.dislikevalue;
+          post.ownlike = 0;
+        }
+      });
+    }
+  }
+
+
+  commentPost(post)
+ {
+   this.navCtrl.push( CommentpetPage, {product_id:post.product_id})
+ }
+
+
+ sharePost(post) {
+  //this code is to use the social sharing plugin
+  // message, subject, file, url
+  this.socialSharing.share(post.description, post.title, '', 'https://whospets.com/zh/shop/products/'+post.product_id)
+  .then(() => {
+    console.log('Success!');
+  })
+  .catch(() => {
+     console.log('Error');
+  });
+}
 }
