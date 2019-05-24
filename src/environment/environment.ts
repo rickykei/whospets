3cbc8b2052353ca90f3cbe1ad1b3e529fcd4d054
wkying <joanne0802@gmail.com>
2019-05-24 16:54:13 +0800

    firebase account

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -3,12 +3,18 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyCHjrxeH8VUADKLhR5Csa54cqPRMDr4Pw8",
-    authDomain: "rosy-dynamics-171007.firebaseapp.com",
-    databaseURL: "https://rosy-dynamics-171007.firebaseio.com",
-    projectId: "rosy-dynamics-171007",
-    storageBucket: "rosy-dynamics-171007.appspot.com",
-    messagingSenderId: "303898922125"
+    // apiKey: "AIzaSyCHjrxeH8VUADKLhR5Csa54cqPRMDr4Pw8",
+    // authDomain: "rosy-dynamics-171007.firebaseapp.com",
+    // databaseURL: "https://rosy-dynamics-171007.firebaseio.com",
+    // projectId: "rosy-dynamics-171007",
+    // storageBucket: "rosy-dynamics-171007.appspot.com",
+    // messagingSenderId: "303898922125"
+      apiKey: "AIzaSyCrsNQHPCSdhzOYXuPykDaB6AWXf0nYcY0",
+      authDomain: "whospets-65e90.firebaseapp.com",
+      databaseURL: "https://whospets-65e90.firebaseio.com",
+      projectId: "whospets-65e90",
+      storageBucket: "whospets-65e90.appspot.com",
+      messagingSenderId: "599906333523"  
   },
   google_web_client_id: "1092390853283-i98feg7fb1dlsm92kkcbim62855pepi8.apps.googleusercontent.com",
   facebook_app_id: 826720427470540,
