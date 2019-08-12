e9b4ab2133de5cee9a0229317a86a0b566854505
wkying <joanne0802@gmail.com>
2019-08-12 15:38:18 +0800

    update

--- buildapp.bat
+++ buildapp.bat
@@ -8,8 +8,13 @@ ionic cordova build android --prod --release
 
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore com.whospets.aos.keystore D:\GitHub\whospets\platforms\android\build\outputs\apk\android-release-unsigned.apk com.whospets.aos
 globetrf
+-- joanne:
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore C:\Users\joanne.wong\Desktop\Joanne-Github\whospets\com.whospets.aos.keystore C:\Users\joanne.wong\Desktop\Joanne-Github\whospets\platforms\android\app\build\outputs\apk\release\app-release-unsigned.apk com.whospets.aos
+
 
 cd D:\GitHub\whospets\platforms\android\build\outputs\apk
 zipalign -v 4 android-release-unsigned.apk whospets.apk
+--joanne
+C:\sdk-tools-windows\build-tools\28.0.3\zipalign.exe -v 4 android-release-unsigned.apk whospets.apk
 cd d:\GitHub\whospets
 pause
