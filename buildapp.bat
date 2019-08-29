42ad669d64a519095e8b7b02d1ea1590da7e1ef2
abc <rickykei@yahoo.com.hk>
2019-08-30 00:30:24 +0800

    20190830
    20190830

--- buildapp.bat
+++ buildapp.bat
@@ -2,6 +2,7 @@
 
 #$ keytool -genkey -v -keystore com.whospets.aos.keystore -alias com.whospets.aos -keyalg RSA -keysize 2048 -validity 10000
 
+keytool -genkey -v -keystore com.nfctouch.whospets.aos.keystore -alias com.nfctouch.whospets.aos -keyalg RSA -keysize 2048 -validity 10000
 
 ionic cordova build android --release
 ionic cordova build android --prod --release
@@ -12,9 +13,10 @@ globetrf
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore C:\Users\joanne.wong\Desktop\Joanne-Github\whospets\com.whospets.aos.keystore C:\Users\joanne.wong\Desktop\Joanne-Github\whospets\platforms\android\app\build\outputs\apk\release\app-release-unsigned.apk com.whospets.aos
 -- joanne macbook:
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore com.whospets.aos.keystore platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk com.whospets.aos
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore com.nfctouch.whospets.aos.keystore platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk com.nfctouch.whospets.aos
 
-cd D:\GitHub\whospets\platforms\android\build\outputs\apk
-zipalign -v 4 android-release-unsigned.apk whospets.apk
+cd D:\GitHub\whospets\platforms\android\app\build\outputs\apk\release
+zipalign -v 4 app-release-unsigned.apk whospets.apk
 --joanne
 C:\sdk-tools-windows\build-tools\28.0.3\zipalign.exe -v 4 android-release-unsigned.apk whospets.apk
 cd d:\GitHub\whospets
