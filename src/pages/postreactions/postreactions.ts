91e32627033b3abf1277babdffb6d284aeb8612f
wkying <joanne0802@gmail.com>
2019-10-10 14:06:52 +0800

    Update postreactions.ts

--- src/pages/postreactions/postreactions.ts
+++ src/pages/postreactions/postreactions.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams, ViewController, Events } from 'ionic-angular';
+import { HttpHeaders, HttpClient } from '../../../node_modules/@angular/common/http';
+import { ResponseModel } from '../profile/profile.model';
 
 /**
  * Generated class for the PostreactionsPage page.
@@ -14,16 +16,80 @@ import { NavController, NavParams, ViewController, Events } from 'ionic-angular'
 })
 export class PostreactionsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,  private viewCtrl: ViewController, public event: Events) {
-  }
+  postResponse:ResponseModel;
+
+  user_id:String;
+  content_id:String;
+  app_table:String;
+
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,  
+    private viewCtrl: ViewController, 
+    public http: HttpClient,  
+    public event: Events) {
+
+      console.log(this.navParams.data);
+      this.user_id = this.navParams.get('user_id');
+      this.content_id = this.navParams.get('content_id');
+      this.app_table = this.navParams.get('app_table');
+
+    }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PostreactionsPage');
   }
 
   report(){
-    this.event.publish('user:back');
-    this.viewCtrl.dismiss();
+    this.addReport();
+    // this.event.publish('user:back');
+    // this.viewCtrl.dismiss();
   }
 
+  addReport() {
+
+   
+    var url;
+    url = "http://api.whospets.com/api/users/set_filter_content.php";       
+
+    let headers = new HttpHeaders();
+    headers.append('Content-Type', 'application/json');
+    headers.append('Access-Control-Allow-Origin' , '*');
+    headers.append('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT');
+    //let options = new RequestOptions({ headers: headers });
+    
+    let data=JSON.stringify({user_id:this.user_id,content_id:this.content_id, 
+      app_table: this.app_table
+    });
+
+    this.http.post(url,data, { headers: headers })
+    // .map(res => res.json(data))
+    //.subscribe(res => {
+      .subscribe((res:ResponseModel) => { 
+        this.postResponse = res; 
+      
+      console.log("VALUE RECEIVED: "+res);
+      if(this.postResponse.success==='true')
+      {
+        this.event.publish('user:back');
+        this.viewCtrl.dismiss();
+      }
+      else
+      {
+        alert("Fail to report : " + this.postResponse.data.message)
+        this.viewCtrl.dismiss();
+      }
+
+    }, (err) => {
+      this.viewCtrl.dismiss();
+      alert("Fail to report, please try it later.")
+    }, () =>
+    {
+      this.viewCtrl.dismiss();
+    });
+  }
+
+
+ 
 }
